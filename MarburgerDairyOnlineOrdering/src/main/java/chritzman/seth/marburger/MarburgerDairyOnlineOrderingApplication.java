8e5e8f564f2a0fb6e74aab6ea7271f41db0b5751
sethc <sethc@DESKTOP-EL1ITLK>
2024-03-08 12:30:01 -0500

    Init

---
+++ MarburgerDairyOnlineOrdering/src/main/java/chritzman/seth/marburger/MarburgerDairyOnlineOrderingApplication.java
@@ -0,0 +1,13 @@
+package chritzman.seth.marburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarburgerDairyOnlineOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarburgerDairyOnlineOrderingApplication.class, args);
+	}
+
+}
---
+++ MarburgerDairyOnlineOrdering/src/test/java/chritzman/seth/marburger/MarburgerDairyOnlineOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package chritzman.seth.marburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarburgerDairyOnlineOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
